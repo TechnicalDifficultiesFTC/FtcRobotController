c6f0e7aa478f48c9cac9ec0267365d697f395d4b
JakobConner7034 <connerj@wlhs.wlwv.k12.or.us>
2021-01-01 22:49:12 -0800

    Update MiscUtils.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/technicaldifficulties/MiscUtils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/technicaldifficulties/MiscUtils.java
@@ -3,7 +3,8 @@ package org.firstinspires.ftc.teamcode.technicaldifficulties;
 public class MiscUtils {
 
     public static String getMotorPowerAsPercentage(double power) {
-        return ((int) (power * 100)) + "%";
+        //return ((int) (power * 100)) + "%";
+        return (power * 100) + "%";
     }
 
 }
